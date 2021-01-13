6f3981a3a25976f9fc94f12f120dbaa930551044
vagivagi <hagikazu7@gmail.com>
2021-01-13 23:53:38 +0900

    Add RequestBuilder

--- src/main/java/com/vagivagi/connector/toggl/TogglWrapperEntryRequest.java
+++ src/main/java/com/vagivagi/connector/toggl/TogglWrapperEntryRequest.java
@@ -1,10 +1,20 @@
 package com.vagivagi.connector.toggl;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(builder = TogglWrapperEntryRequestBuilder.class)
 public class TogglWrapperEntryRequest {
     private String description;
     private int wid;
     private int pid;
     private String key;
+
+    public TogglWrapperEntryRequest(String description, int wid, int pid, String key) {
+        this.description = description;
+        this.wid = wid;
+        this.pid = pid;
+        this.key = key;
+    }
 
     public String getDescription() {
         return description;
