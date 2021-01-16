86266a28b18ec122e878ceeab22cb93f70e2b4af
vagivagi <hagikazu7@gmail.com>
2021-01-16 16:55:44 +0900

    Update TogglWrapperHandlerTest.java
    mockに変更

--- src/test/java/com/vagivagi/connector/toggl/TogglWrapperHandlerTest.java
+++ src/test/java/com/vagivagi/connector/toggl/TogglWrapperHandlerTest.java
@@ -1,34 +1,41 @@
 package com.vagivagi.connector.toggl;
 
+import com.vagivagi.connector.common.ConnectorResponseBody;
 import com.vagivagi.connector.ifttt.IftttService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TogglWrapperHandlerTest {
     WebTestClient client;
 
-    @Autowired
-    RouterFunction<ServerResponse> routerFunction;
-    @Autowired
+    @Mock
     private TogglWrapperService togglWrapperService;
-    @Autowired
+    @Mock
     private TogglWrapperVerifier togglWrapperVerifier;
-    @Autowired
+    @Mock
     private IftttService iftttService;
 
     @BeforeEach
     public void setUp() {
+        TogglWrapperHandler togglWrapperHandler =
+                new TogglWrapperHandler(togglWrapperService, togglWrapperVerifier, iftttService);
+        RouterFunction routerFunction = togglWrapperHandler.routes();
+        when(togglWrapperService.start("description", 0))
+                .thenReturn(Mono.just(ConnectorResponseBody.builder()
+                        .message("success").body(new TogglTimeEntryResponseBody()).build()));
+        when(togglWrapperService.getTimeEntryFromPastRecord("description", 0))
+                .thenReturn(Mono.just(new TogglTimeEntry()));
+        // when(iftttService.triggerLightChange()).thenReturn(Mono.just("success"));
         client = WebTestClient.bindToRouterFunction(routerFunction).configureClient().build();
     }
 
@@ -38,11 +45,9 @@ public class TogglWrapperHandlerTest {
                 .post()
                 .uri("/toggl/start")
                 .accept(MediaType.APPLICATION_JSON)
-                .body(Mono.just(new TogglWrapperEntryRequest("", 0, 0, "test-key")), TogglWrapperEntryRequest.class)
+                .body(Mono.just(new TogglWrapperEntryRequest("description", 0, 0, "test-key")), TogglWrapperEntryRequest.class)
                 .exchange()
                 .expectStatus()
-                .isCreated()
-                .expectHeader()
-                .valueEquals("Location", "");
+                .isOk();
     }
 }
