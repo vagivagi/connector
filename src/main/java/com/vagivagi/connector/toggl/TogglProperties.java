bdcbed99e057bf1af590a23aeddfadc40f09a33e
vagivagi <hagikazu7@gmail.com>
2021-02-11 21:47:38 +0900

    Update TogglProperties.java

--- src/main/java/com/vagivagi/connector/toggl/TogglProperties.java
+++ src/main/java/com/vagivagi/connector/toggl/TogglProperties.java
@@ -9,17 +9,23 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 public class TogglProperties {
 
     private final String baseUrl;
+    private final String reportBaseUrl;
     private final String apiToken;
     private final String myKey;
 
-    public TogglProperties(String baseUrl, String apiToken, String myKey) {
+    public TogglProperties(String baseUrl, String reportBaseUrl, String apiToken, String myKey) {
         this.baseUrl = baseUrl;
+        this.reportBaseUrl = reportBaseUrl;
         this.apiToken = apiToken;
         this.myKey = myKey;
     }
 
     public String getBaseUrl() {
         return baseUrl;
+    }
+
+    public String getReportBaseUrl() {
+        return reportBaseUrl;
     }
 
     public String getApiToken() {
