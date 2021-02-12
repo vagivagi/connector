1530ab9111318cdb76c0c755d013f49659516aa0
vagivagi <hagikazu7@gmail.com>
2021-02-12 23:08:32 +0900

    Update IftttEventEnum.java

--- src/main/java/com/vagivagi/connector/ifttt/IftttEventEnum.java
+++ src/main/java/com/vagivagi/connector/ifttt/IftttEventEnum.java
@@ -1,7 +1,8 @@
 package com.vagivagi.connector.ifttt;
 
 public enum IftttEventEnum {
-    LIGHT_0N("light_on", "light on by switchbot");
+    LIGHT_0N("light_on", "light on by switchbot"),
+    STUDY_REPORT("study_report", "English Study Report Notification");
 
     IftttEventEnum(String name, String description) {
         this.name = name;
