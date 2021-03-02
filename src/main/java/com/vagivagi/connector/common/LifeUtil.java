f231db37fe4bfb4336ee24864a75034ce36c5320
vagivagi <hagikazu7@gmail.com>
2021-03-02 21:11:15 +0900

    Update LifeUtil.java

--- src/main/java/com/vagivagi/connector/common/LifeUtil.java
+++ src/main/java/com/vagivagi/connector/common/LifeUtil.java
@@ -7,8 +7,9 @@ import java.time.ZoneId;
 
 @Component
 public class LifeUtil {
+    public static final ZoneId ZONE_TOKYO = ZoneId.of("Asia/Tokyo");
     public boolean isSleeping() {
-        int nowHour = LocalDateTime.now(ZoneId.of("Asia/Tokyo")).getHour();
+        int nowHour = LocalDateTime.now(ZONE_TOKYO).getHour();
         return 0 <= nowHour && nowHour <= 7;
     }
 }
